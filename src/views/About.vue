e405ef4ab5ace2d436a7d1330e69829946b6d149
manoy96 <mfewcar@gmail.com>
2019-05-07 22:22:56 -0600

    creating a banner

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,21 @@
 <template>
   <div class="about">
-    
+    <v-container fluid class="error myBanner">
+      <v-layout justify-start align-center >
+        <h2 class="display-3">Content</h2>
+      </v-layout>
+    </v-container>
   </div>
 </template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style lang="scss" scoped>
+.myBanner {
+  height: 95vh;
+}
+</style>
