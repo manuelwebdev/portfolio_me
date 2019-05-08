878c6f387d02438b3508e7441a4ff042076a29a9
manoy96 <mfewcar@gmail.com>
2019-05-07 21:16:52 -0600

    working on about page

---
+++ src/components/Header.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-toolbar app>
+    <v-toolbar-title class="headline text-uppercase">
+      <span class="font-weight-light">Manuel Espinoza</span>
+    </v-toolbar-title>
+    <v-spacer></v-spacer>
+
+    <router-link tag="v-btn" to="/">
+      <v-btn flat class="mx-0">
+        <span>Home</span>
+      </v-btn>
+    </router-link>
+
+    <router-link tag="v-btn" to="/about">
+      <v-btn flat>
+        <span>About</span>
+      </v-btn>
+    </router-link>
+
+    <v-btn flat href="https://github.com/manoy96" target="_blank">
+      <span class="mr-1">GitHub</span>
+    </v-btn>
+    
+  </v-toolbar>
+  
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+</style>
