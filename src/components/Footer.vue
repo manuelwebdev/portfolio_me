bf53d69b3f44d99990263b279ce74d8290971558
manoy96 <mfewcar@gmail.com>
2019-05-16 16:44:25 -0600

    configured Footer to add more links customization

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,19 +1,26 @@
 <template>
   <v-footer dark height="auto">
     <v-card class="flex" flat tile>
-      <v-card-title class="teal">
-        <strong class="subheading">Get connected with us on social networks!</strong>
-
+      <v-card-title class="secondary">
+        <strong class="subheading">Find me on my socials!</strong>
         <v-spacer></v-spacer>
-
-        <v-btn v-for="icon in icons" :key="icon" class="mx-3" dark icon>
-          <v-icon size="24px">{{ icon }}</v-icon>
+        <v-btn large icon flat href="https://www.instagram.com/mespinut/" target="_blank">
+            <i class="fab fa-instagram fa-2x"></i>
+        </v-btn>
+        <v-btn large icon flat href="https://twitter.com/mespinUT" target="_blank">
+            <i class="fab fa-twitter fa-2x"></i>
+        </v-btn>
+        <v-btn large icon flat href="https://www.linkedin.com/in/mespinut/" target="_blank">
+            <i class="fab fa-linkedin-in fa-2x"></i>
+        </v-btn>
+        <v-btn large icon flat href="https://github.com/manoy96" target="_blank">
+            <i class="fab fa-github"></i>
         </v-btn>
       </v-card-title>
 
-      <v-card-actions class="grey darken-3 justify-center">
+      <v-card-actions class="primary justify-center">
         &copy;2018 —
-        <strong>Vuetify</strong>
+        <strong>Manuel Espinoza</strong>
       </v-card-actions>
     </v-card>
   </v-footer>
@@ -23,9 +30,7 @@
 export default {
      data: () => ({
       icons: [
-        'fab fa-facebook',
         'fab fa-twitter',
-        'fab fa-google-plus',
         'fab fa-linkedin',
         'fab fa-instagram'
       ]
