85e387c2a12e9dafada8e69ba6a0ce163270dba6
manoy96 <mfewcar@gmail.com>
2019-05-27 22:41:04 -0600

    adding chips to about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,6 +1,14 @@
 <template>
   <div class="about">
-    
+    <div class="text-xs-center">
+    <v-chip color="primary" text-color="white">Primary</v-chip>
+
+    <v-chip color="secondary" text-color="white">Secondary</v-chip>
+
+    <v-chip color="red" text-color="white">Colored Chip</v-chip>
+
+    <v-chip color="green" text-color="white">Colored Chip</v-chip>
+  </div>
   </div>
 </template>
 
