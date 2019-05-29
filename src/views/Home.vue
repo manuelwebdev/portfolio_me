9ac2070285ced0dc2bd05de56651637a5e7dd306
manoy96 <mfewcar@gmail.com>
2019-05-28 22:45:43 -0600

    changes

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -40,8 +40,9 @@
               </div>
             </v-card-title>
             <v-card-actions>
-              <v-btn flat color="orange">Share</v-btn>
-              <v-btn flat color="orange">Explore</v-btn>
+              <v-layout column align-content-end>
+                <v-btn flat color="orange">Explore</v-btn>
+              </v-layout>
             </v-card-actions>
           </v-card>
         </v-flex>
