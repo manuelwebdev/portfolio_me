350c53a95331e8ceab0dc11d344a4ff15850b91d
manoy96 <mfewcar@gmail.com>
2019-06-08 23:49:06 -0600

    adding items to timeline setup

--- src/views/About.vue
+++ src/views/About.vue
@@ -11,12 +11,21 @@
     </div>
     <v-container>
       <v-timeline>
-        <v-timeline-item v-for="n in 4" :key="n" color="red lighten-2" large>
-          <template v-slot:opposite>
-            <span>Tus eu perfecto</span>
-          </template>
+        <v-timeline-item left color="red lighten-2" large>
           <v-card class="elevation-2">
             <v-card-title class="headline">My Life</v-card-title>
+            <v-card-text>Lorem ipsum dolor sit amet, no nam oblique veritus. Commune scaevola imperdiet nec ut, sed euismod convenire principes at. Est et nobis iisque percipit, an vim zril disputando voluptatibus, vix an salutandi sententiae.</v-card-text>
+          </v-card>
+        </v-timeline-item>
+        <v-timeline-item right color="blue lighten-2" large>
+          <v-card class="elevation-2">
+            <v-card-title class="headline">My Hobbies</v-card-title>
+            <v-card-text>Lorem ipsum dolor sit amet, no nam oblique veritus. Commune scaevola imperdiet nec ut, sed euismod convenire principes at. Est et nobis iisque percipit, an vim zril disputando voluptatibus, vix an salutandi sententiae.</v-card-text>
+          </v-card>
+        </v-timeline-item>
+        <v-timeline-item left color="green lighten-2" large>
+          <v-card class="elevation-2">
+            <v-card-title class="headline">My Goals</v-card-title>
             <v-card-text>Lorem ipsum dolor sit amet, no nam oblique veritus. Commune scaevola imperdiet nec ut, sed euismod convenire principes at. Est et nobis iisque percipit, an vim zril disputando voluptatibus, vix an salutandi sententiae.</v-card-text>
           </v-card>
         </v-timeline-item>
