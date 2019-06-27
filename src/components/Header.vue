485ad7213a264c103138573cc9af9d154812efef
manoy96 <mfewcar@gmail.com>
2019-06-26 23:19:48 -0600

    trying to add placeholder logo to header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,7 +2,11 @@
   <v-toolbar app fixed class="primary">
     <!-- <v-toolbar-title class="headline text-uppercase">
       <span class="font-weight-light white--text">Manuel Espinoza</span>
-    </v-toolbar-title> -->
+    </v-toolbar-title>-->
+    <v-avatar slot="icon">
+      <!-- <img src="http://i.pravatar.cc/64"> -->
+      <i class="fas fa-code fa-2x black--text"></i>
+    </v-avatar>
     <v-spacer></v-spacer>
 
     <router-link tag="v-btn" to="/">
@@ -18,11 +22,11 @@
     </router-link>
 
     <v-btn flat href="https://github.com/manoy96" target="_blank">
-      <span class="mr-1 white--text"><i class="fab fa-github fa-lg"></i> GitHub</span>
+      <span class="mr-1 white--text">
+        <i class="fab fa-github fa-lg"></i> GitHub
+      </span>
     </v-btn>
-    
   </v-toolbar>
-  
 </template>
 
 <script>
