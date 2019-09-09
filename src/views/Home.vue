f745a16a56bc920d06183b8e9391ec97dab1b96d
manoy96 <mfewcar@gmail.com>
2019-09-08 23:23:08 -0600

    comparing changes to skills bar to previous version

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -29,6 +29,38 @@
               <div class="accent lighten-2 right black--text font-weight-light">90%</div>
             </div>
           </div>
+
+          <div class="skills">
+            <div class="primary lighten-2 title">HTML</div>
+            <div class="primary bar">
+              <div class="left"></div>
+              <div class="accent lighten-2 black--text right">90%</div>
+            </div>
+          </div>
+
+          <div class="skills">
+            <div class="primary lighten-2 title">HTML</div>
+            <div class="primary bar">
+              <div class="left"></div>
+              <div class="accent lighten-2 black--text right">90%</div>
+            </div>
+          </div>
+
+          <div class="skills">
+            <div class="primary lighten-2 title">HTML</div>
+            <div class="primary bar">
+              <div class="left"></div>
+              <div class="accent lighten-2 black--text right">90%</div>
+            </div>
+          </div>
+
+          <div class="skills">
+            <div class="primary lighten-2 title">HTML</div>
+            <div class="primary bar">
+              <div class="left"></div>
+              <div class="accent lighten-2 black--text right">90%</div>
+            </div>
+          </div>
         </v-flex>
       </v-layout>
       <v-timeline>
@@ -71,37 +103,7 @@
             </div>
           </div>
 
-          <div class="skills">
-            <div class="primary lighten-2 title">HTML</div>
-            <div class="primary bar">
-              <div class="left"></div>
-              <div class="accent lighten-2 black--text right">90%</div>
-            </div>
-          </div>
-
-          <div class="skills">
-            <div class="primary lighten-2 title">HTML</div>
-            <div class="primary bar">
-              <div class="left"></div>
-              <div class="accent lighten-2 black--text right">90%</div>
-            </div>
-          </div>
-
-          <div class="skills">
-            <div class="primary lighten-2 title">HTML</div>
-            <div class="primary bar">
-              <div class="left"></div>
-              <div class="accent lighten-2 black--text right">90%</div>
-            </div>
-          </div>
-
-          <div class="skills">
-            <div class="primary lighten-2 title">HTML</div>
-            <div class="primary bar">
-              <div class="left"></div>
-              <div class="accent lighten-2 black--text right">90%</div>
-            </div>
-          </div>
+          
         </v-flex>
       </v-layout>
     </v-container>
